00092623ace7e8a1a75ab16d9e210583aa11d1d8
connor <connor.chaplain@pipemind.com>
2020-07-24 21:35:03 +0100

    test

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
-import express from 'express';
-import {port} from './config';
+import express from "express";
+import {port} from "./config";
 
-import { handler } from './controller.js'
+import { handler } from "./controller.js";
+
+const db = process.env.MONGODB_URL;
 
 const app = express();
 
-app.get('/', handler);
+app.get("/", handler);
 
 app.listen(port, () => console.log(
     `Example app listening on port ${port}!`
 ));
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+connectDB()
