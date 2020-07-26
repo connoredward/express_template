b461526ec32381bf1c35b0732498b0eae5b48b31
connor <connor.chaplain@pipemind.com>
2020-07-26 15:22:36 +0100

    adding docker

--- src/config.js
+++ src/config.js
@@ -5,4 +5,7 @@ dotenv.config();
 module.exports = {
   masterKey: process.env.API_KEY,
   port: process.env.PORT,
+  mongo: {
+    uri: process.env.MONGODB_URL || 'mongodb://mongo:27017/api'
+  }
 };
--- src/server.js
+++ src/server.js
@@ -1,33 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
 
-import { port } from "./config";
+import { port, mongo } from "./config";
 import { handler } from "./controller.js";
 
-
-const db = process.env.MONGODB_URL;
+// const db = process.env.MONGODB_URL;
 
 const app = express();
 
 app.get("/", handler);
 
+app.get("/test", (req, res) => res.send('Hello world'))
+
 app.listen(port, () => console.log(
     `Example app listening on port ${port}!`
 ));
 
-
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
-    console.log("MongoDB is Connected...");
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(mongo.uri, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        console.log("MongoDB is Connected...");
+    } catch (err) {
+        console.error(err.message);
+        // process.exit(1);
+    }
 };
-
 
 connectDB()
